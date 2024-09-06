29a99b5e507f142b0fbdf326e82b20ce3ff04853
rantis-f <fayyadrantisismktibazma@gmail.com>
2024-09-06 08:40:55 +0700

    Penyesuaian tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  purge: [
+    './*.html',
+  ],
+   darkMode: true,
+   theme: {
+     extend: {}
+   },
+   variants: {},
+   plugins: [],
+ }
